a3a7298f2b836c7fb481341d5b29accf3782fd3d
Zablon-Onchari-Nyabuto <zablon.nyabuto@student.moringaschool.com>
2022-10-29 20:14:35 +0300

    Updated code challenge

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,12 +1,57 @@
 class Magazine
   attr_accessor :name, :category
-
+  
+  @@all = []
 
   def initialize(name, category)
     @name = name
     @category = category
+    @authors = []
+    @@all << self
 
   end
 
+  def self.all
+    @@all
+  end
 
+  def contributors
+    articles = Article.all.filter do |article|
+    self == article.magazine
+    end
+
+    articles.map do |article|
+      article.author
+    end
+  end
+
+  def self.find_by_name(name)
+    all.find {|magazine| magazine.name == name}
+  end
+
+  def article_titles
+    articles = Article.all.filter do |article|
+      self == article.magazine
+      end
+  
+      articles.map do |article|
+        article.title
+      end
+    end
+
+    def contributing_authors
+      articles = Article.all.filter do |article|
+        self == article.magazine
+        end
+    
+        all_authors = articles.map do |article|
+          article.author 
+        end
+
+        uniq_authors = all_authors.uniq 
+        uniq_authors.filter do |author|
+           all_authors.count(author) > 2
+        end
+    end
 end
+
--- tools/console.rb
+++ tools/console.rb
@@ -6,7 +6,11 @@ end
 
 
 ###  WRITE YOUR TEST CODE HERE ###
-
+author = Author.new("Ken Walibora")
+magazine = Magazine.new("The Sun", "Cjhuhuh")
+article1 = Article.new(author, magazine, "New string")
+article2 = Article.new(author, magazine, "Newark station")
+article3 = Article.new(author, magazine, "Chihuahua")
 
 
 
